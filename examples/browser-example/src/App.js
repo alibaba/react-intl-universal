8125568a25bc69e6547c9bfe03925f2410a022f6
Daniel Nascimento <danascimen@thoughtworks.com>
2018-10-13 22:32:40 -0300

    Adding pt-BR in node-example and browser-example

--- examples/browser-example/src/App.js
+++ examples/browser-example/src/App.js
@@ -30,6 +30,10 @@ const SUPPOER_LOCALES = [
   {
     name: "日本の",
     value: "ja-JP"
+  },
+  {
+    name: "Portuguese",
+    value: "pt-BR"
   }
 ];
 
@@ -65,6 +69,7 @@ class App extends Component {
       urlLocaleKey: "lang",
       cookieLocaleKey: "lang"
     });
+    
     if (!_.find(SUPPOER_LOCALES, { value: currentLocale })) {
       currentLocale = "en-US";
     }
@@ -85,6 +90,8 @@ class App extends Component {
         // After loading CLDR locale data, start to render
         this.setState({ initDone: true });
       });
+      console.log('teste')
+
   }
 
   renderLocaleSelector() {
---
+++ test/locales/pt-BR.js
@@ -0,0 +1,12 @@
+module.exports = ({
+    "SIMPLE": "Simples",
+    "HELLO": "Olá, {name}",
+    "TIP": "Isso é <span>HTML</span>",
+    "TIP_VAR": "Isso é <span>{message}</span>",
+    "SALE_START": "A venda começa {start, date}",
+    "SALE_END": "A venda termina {end, date, long}",
+    "COUPON": "O cupom expira em {expires, time, medium}",
+    "SALE_PRICE": "O preço é {price, number, BRL}",
+    "PHOTO": "Você tem {num, plural, =0 {nenhuma foto} =1 {uma foto} outro {# fotos}}",
+    "MESSAGE_NOT_IN_COMPONENT": "react-intl-universal não habilita messagem de internacionalização no React.Component"
+});

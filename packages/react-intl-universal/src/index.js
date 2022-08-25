1b10b47c3e788407448735f05a48f4903c81c3fc
寞然 <chenhaojie.chj@alibaba-inc.com>
2022-08-25 21:37:06 +0800

    chore: add semicolon

--- packages/react-intl-universal/src/index.js
+++ packages/react-intl-universal/src/index.js
@@ -2,7 +2,7 @@ import ReactIntlUniversal from './ReactIntlUniversal';
 
 const defaultInstance = new ReactIntlUniversal();
 // resolved by CommonJS module loader
-defaultInstance.ReactIntlUniversal = ReactIntlUniversal
+defaultInstance.ReactIntlUniversal = ReactIntlUniversal;
 export default defaultInstance;
 // resolved by ECMAScript module loader
 export { ReactIntlUniversal };

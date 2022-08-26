de6ad04d6d382b487de01d0ed2a617c127fe1a98
hj <chenhaojie.chj@alibaba-inc.com>
2022-08-26 14:01:24 +0800

    feat: export `ReactIntlUniversal` for ECMAScript module loader (#191)
    * feat: support ECMAScript module format
    
    * fix: export ReactIntlUniversal in namespace
    
    * chore: add semicolon

--- packages/react-intl-universal/src/index.js
+++ packages/react-intl-universal/src/index.js
@@ -1,5 +1,8 @@
 import ReactIntlUniversal from './ReactIntlUniversal';
 
 const defaultInstance = new ReactIntlUniversal();
-defaultInstance.ReactIntlUniversal = ReactIntlUniversal
+// resolved by CommonJS module loader
+defaultInstance.ReactIntlUniversal = ReactIntlUniversal;
 export default defaultInstance;
+// resolved by ECMAScript module loader
+export { ReactIntlUniversal };

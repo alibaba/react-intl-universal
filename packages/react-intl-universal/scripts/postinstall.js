17263713cc45e23fa405765058eb7bc705bc8356
清学 <chiawei.tcw@alibaba-inc.com>
2022-11-05 00:14:38 +0800

    chore: post message to v2.6.11

---
+++ packages/react-intl-universal/scripts/postinstall.js
@@ -0,0 +1,10 @@
+
+try {
+  const chalk = require('chalk');
+  console.log('');
+  console.log(chalk.bold('Efficient way to extract i18n locale messages, use ') + chalk.bold.magenta('react-intl-universal-extract') + ':');
+  console.log(chalk.cyan('https://github.com/alibaba/react-intl-universal/tree/master/packages/react-intl-universal-extract'));
+  console.log('');
+} catch (error) {
+  // don't print message even if there's an error
+}

b74b06bebf791244bf64bb59d1df631c6c12803f
清学 <chiawei.tcw@alibaba-inc.com>
2023-03-11 11:44:26 +0800

    chore: post message

--- packages/react-intl-universal/scripts/postinstall.js
+++ packages/react-intl-universal/scripts/postinstall.js
@@ -2,8 +2,10 @@
 try {
   const chalk = require('chalk');
   console.log('');
-  console.log(chalk.bold('Efficient way to extract i18n locale messages, use ') + chalk.bold.magenta('react-intl-universal-extract') + ':');
+  console.log(chalk.bold.magenta('[react-intl-universal-extract] Extract i18n locale messages efficiently:'));
   console.log(chalk.cyan('https://github.com/alibaba/react-intl-universal/tree/master/packages/react-intl-universal-extract'));
+  console.log(chalk.bold.magenta('[react-intl-universal] Live Demo:'));
+  console.log(chalk.cyan('https://fe-tool.com/react-intl-universal'));
   console.log('');
 } catch (error) {
   // don't print message even if there's an error
